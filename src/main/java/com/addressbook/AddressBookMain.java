ccf4708ded72e385d4c32270dc15d0cacb868a25
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-03-07 19:50:20 +0530

    [Shifa khan] : UC6 - Ability to add multiple AddressBooks using HashMap

--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -8,19 +8,15 @@ public class AddressBookMain {
 
         Scanner sc = new Scanner(System.in);
 
-        AddressBook addressBook = new AddressBook();
-
-        System.out.println("Welcome to Address Book Program");
+        AddressBookSystem system = new AddressBookSystem();
 
         while(true) {
 
-            System.out.println("\n1. Add Contact");
-            System.out.println("2. Display Contacts");
-            System.out.println("3. Edit Contact");
-            System.out.println("4. Delete Contact");
-            System.out.println("5. Exit");
+            System.out.println("\n1. Add AddressBook");
+            System.out.println("2. Display AddressBooks");
+            System.out.println("3. Exit");
 
-            System.out.print("Enter your choice: ");
+            System.out.print("Enter choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
 
@@ -28,63 +24,26 @@ public class AddressBookMain {
 
                 case 1:
 
-                    System.out.println("Enter First Name:");
-                    String firstName = sc.nextLine();
+                    System.out.println("Enter AddressBook Name:");
 
-                    System.out.println("Enter Last Name:");
-                    String lastName = sc.nextLine();
+                    String name = sc.nextLine();
 
-                    System.out.println("Enter Address:");
-                    String address = sc.nextLine();
+                    AddressBook addressBook = new AddressBook();
 
-                    System.out.println("Enter City:");
-                    String city = sc.nextLine();
-
-                    System.out.println("Enter State:");
-                    String state = sc.nextLine();
-
-                    System.out.println("Enter Zip:");
-                    String zip = sc.nextLine();
-
-                    System.out.println("Enter Phone:");
-                    String phone = sc.nextLine();
-
-                    System.out.println("Enter Email:");
-                    String email = sc.nextLine();
-
-                    Contact contact = new Contact(firstName,lastName,address,city,state,zip,phone,email);
-
-                    addressBook.addContact(contact);
+                    system.addAddressBook(name, addressBook);
 
                     break;
 
                 case 2:
 
-                    addressBook.displayContacts();
+                    system.displayAddressBooks();
 
                     break;
 
                 case 3:
 
-                    System.out.println("Enter name to edit:");
-                    String editName = sc.nextLine();
+                    System.out.println("Exiting...");
 
-                    addressBook.editContact(editName);
-
-                    break;
-
-                case 4:
-
-                    System.out.println("Enter name to delete:");
-                    String deleteName = sc.nextLine();
-
-                    addressBook.deleteContact(deleteName);
-
-                    break;
-
-                case 5:
-
-                    System.out.println("Exiting program...");
                     return;
 
                 default:
