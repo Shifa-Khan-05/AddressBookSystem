e3c74285cb46bcbec017a6feb3d9fda999298d25
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-03-07 15:04:37 +0530

    UC1 - Ability to create contact in Address Book

--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -2,8 +2,21 @@ package com.addressbook;
 
 public class AddressBookMain {
 
-	public static void main(String[] args) {
-		
-		 System.out.println("Welcome to Address Book Program");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+
+        Contact contact = new Contact(
+                "Shifa",
+                "Khan",
+                "Bhopal",
+                "Bhopal",
+                "MP",
+                "462001",
+                "9876543210",
+                "shifa@gmail.com"
+        );
+
+        contact.displayContact();
+    }
 }
---
+++ src/main/java/com/addressbook/Contact.java
@@ -0,0 +1,39 @@
+package com.addressbook;
+
+public class Contact {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phoneNumber, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public void displayContact() {
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip: " + zip);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Email: " + email);
+    }
+}
